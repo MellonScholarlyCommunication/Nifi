9be2610030ed30d57373a25793865228632ba418
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2021-09-16 15:27:54 +0200

    Adding the Refresher

---
+++ web/src/Refresher.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { resetCount } from './stores.js';
+
+    function refreshAll() {
+        resetCount.update(n => n + 1);
+        return true;
+    }
+</script>
+
+<button on:click="{refreshAll}">Refresh All</button>
